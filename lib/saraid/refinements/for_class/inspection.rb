dc42fc09e117408850dc773958e703c5d0772c5b
Michael Chui <saraid@users.noreply.github.com>
2023-09-05 23:48:43 -0700

    Pretty print (#1)
    * pretty print attempt; not sure i like it
    
    * better
    
    * some small cleaning

--- lib/saraid/refinements/for_class/inspection.rb
+++ lib/saraid/refinements/for_class/inspection.rb
@@ -19,6 +19,36 @@ module Saraid
 
                 "#<#{self.class}:#{'0x%x' % (object_id << 1)}#{postamble}>"
               end
+
+              def pretty_print(q)
+                attributes_to_inspect =
+                  if respond_to?(:inspectable_attributes)
+                    inspectable_attributes.map do |attr|
+                      next attr.to_s if respond_to?(attr)
+                      next "@#{attr}" if instance_variables.include?(attr)
+                    end
+                  else
+                    instance_variables.sort
+                  end
+
+                q.object_address_group(self) do
+                  q.seplist(attributes_to_inspect, lambda { q.text ',' }) do |ivar|
+                    q.breakable
+                    ivar = ivar.to_s if Symbol === ivar
+                    q.text ivar
+                    q.text '='
+                    q.group(1) {
+                      q.breakable ''
+                      q.pp(instance_eval(ivar))
+                    }
+                  end
+
+                  if respond_to?(:custom_inspection)
+                    q.breakable
+                    q.text custom_inspection
+                  end
+                end
+              end
             end
           end
         end
